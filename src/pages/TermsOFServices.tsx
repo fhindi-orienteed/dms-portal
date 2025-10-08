214864e11453bdb33e7082c617d93fac3b341628
Abeer Kharouf <abeermoath550@gmail.com>
2025-10-08 13:06:38 -0700

    useing component card

--- src/pages/TermsOFServices.tsx
+++ src/pages/TermsOFServices.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 
-import { PageBreadcrumb, PageMeta } from "../components/common";
+import { ComponentCard, PageBreadcrumb, PageMeta } from "../components/common";
 
 export default function TermsOfServices(){
     const { t } = useTranslation(); 
@@ -11,31 +11,35 @@ export default function TermsOfServices(){
                 description="otherLinks.termsOfService"
             />
             <PageBreadcrumb pageTitle="Terms of service" />
-            <h1 className="text-xl font-simi text-gray-800 mb-4">Terms of Service</h1>
-            <div className="container mx-auto p-4 bg-white border border-gray-200 rounded-2xl text-gray-800">
+            <ComponentCard 
+            title="Terms of sevice"
+            desc="Please read these terms and conditions carefully before using our service.">
                 
-                <p className="text-gray-500">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-                    risus vel massa tincidunt aliquam. Curabitur feugiat luctus lorem,
-                    eget facilisis orci tincidunt nec. Morbi sed ipsum non velit varius
-                    gravida. 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-                    risus vel massa tincidunt aliquam. Curabitur feugiat luctus lorem,
-                    eget facilisis orci tincidunt nec. Morbi sed ipsum non velit varius
-                    gravida. 
-                </p>
-                <p className="text-gray-500 mt-4">
-                    Suspendisse potenti. Vivamus eget fermentum nisl. Etiam sit amet nulla
-                    id elit volutpat euismod.
-                </p>
+                <div className="container mx-auto p-4 bg-white border border-gray-200 rounded-2xl text-gray-800">
+                    
+                    <p className="text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
+                        risus vel massa tincidunt aliquam. Curabitur feugiat luctus lorem,
+                        eget facilisis orci tincidunt nec. Morbi sed ipsum non velit varius
+                        gravida. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
+                        risus vel massa tincidunt aliquam. Curabitur feugiat luctus lorem,
+                        eget facilisis orci tincidunt nec. Morbi sed ipsum non velit varius
+                        gravida. 
+                    </p>
+                    <p className="text-gray-500 mt-4">
+                        Suspendisse potenti. Vivamus eget fermentum nisl. Etiam sit amet nulla
+                        id elit volutpat euismod.
+                    </p>
 
-                <p  className="text-gray-500 mt-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-                    risus vel massa tincidunt aliquam. Curabitur feugiat luctus lorem,
-                    eget facilisis orci tincidunt nec. Morbi sed ipsum non velit varius
-                    gravida. 
-                </p>
-            </div>
+                    <p  className="text-gray-500 mt-4">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
+                        risus vel massa tincidunt aliquam. Curabitur feugiat luctus lorem,
+                        eget facilisis orci tincidunt nec. Morbi sed ipsum non velit varius
+                        gravida. 
+                    </p>
+                </div>
+            </ComponentCard>
         </>
 
     );
