bb1c372f5f783a06efe0268571026b265a53a60f
bayan7abdalhq <s12114374@stu.najah.edu>
2025-10-20 20:51:19 +0300

    update code

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -1,21 +1,10 @@
 import { NotificationResponse } from "../models/notificationModel";
-import { apiUtils } from "../utils/apiUtils";
 import api from "../config/api";
 
 export const notificationService = {
   async getNotifications(): Promise<NotificationResponse> {
-    const token = apiUtils.auth.getToken();
-    if (!token) {
-      throw new Error("User is not authenticated");
-    }
-
     try {
-      const response = await api.get("notification/list", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
+      const response = await api.get("notification/list");
       return response.data;
     } catch (error) {
       throw new Error("Failed to fetch notifications");
